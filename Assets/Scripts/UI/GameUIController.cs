4285887a336463d83b9469d259fce7b35fd67816
MisaJovanovic <misaj@nordeus.com>
2021-04-25 22:10:09 +0200

    new tiles, ui controller

---
+++ Assets/Scripts/UI/GameUIController.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Main UI controller.
+/// </summary>
+public class GameUIController : MonoBehaviour
+{
+}
